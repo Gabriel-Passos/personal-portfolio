d9afc19b28decc77efb6c27bfb99d9d0ba2f6039
Gabriel Passos <dev.ghpassos@gmail.com>
2026-08-31 18:02:10 -0300

    feat: add vercel speed insights

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,13 @@ import { RouterView } from "vue-router";
 import Layout from "./components/organisms/layout.vue";
 import BackToTopButton from "./components/molecules/back-to-top-button.vue";
 import { Analytics } from "@vercel/analytics/vue";
+import { SpeedInsights } from "@vercel/speed-insights/vue";
 </script>
 
 <template>
   <Layout>
     <Analytics />
+    <SpeedInsights />
     <RouterView />
     <BackToTopButton />
   </Layout>
